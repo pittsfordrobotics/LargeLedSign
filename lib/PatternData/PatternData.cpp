d7baf7faa34ad02b456c2c37b2d87eaa89b988b0
Larry Moore <lfmoore3@hotmail.com>
2023-12-17 12:24:35 -0500

    Minor refactor of PatternData struct

--- lib/PatternData/PatternData.cpp
+++ lib/PatternData/PatternData.cpp
@@ -6,27 +6,12 @@ PatternData::PatternData()
 
 PatternData::PatternData(const PatternData& other)
 {
-    this->colorPattern = other.colorPattern;
-    this->displayPattern = other.displayPattern;
-    this->param1 = other.param1;
-    this->param2 = other.param2;
-    this->color1 = other.color1;
-    this->color2 = other.color2;
-    this->color3 = other.color3;
-    this->color4 = other.color4;
+    copy(other);
 }
 
 PatternData& PatternData::operator=(const PatternData& other)
 {
-    this->colorPattern = other.colorPattern;
-    this->displayPattern = other.displayPattern;
-    this->param1 = other.param1;
-    this->param2 = other.param2;
-    this->color1 = other.color1;
-    this->color2 = other.color2;
-    this->color3 = other.color3;
-    this->color4 = other.color4;
-
+    copy(other);
     return *this;
 }
 
@@ -53,3 +38,16 @@ bool PatternData::operator!=(const PatternData& other)
         || this->color3 != other.color3
         || this->color4 != other.color4);
 }
+
+void PatternData::copy(const PatternData& other)
+{
+    this->colorPattern = other.colorPattern;
+    this->displayPattern = other.displayPattern;
+    this->param1 = other.param1;
+    this->param2 = other.param2;
+    this->color1 = other.color1;
+    this->color2 = other.color2;
+    this->color3 = other.color3;
+    this->color4 = other.color4;    
+}
+
