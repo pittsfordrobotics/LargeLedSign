bdcf25367cccf1dd861fbc21fcfa5ba3eb503648
Larry Moore <lfmoore3@hotmail.com>
2024-01-08 19:56:45 -0500

    Migrate display patterns to common

--- lib/ColorPatternLib/ColorPatternLib.h
+++ lib/ColorPatternLib/ColorPatternLib.h
@@ -1,6 +1,8 @@
 #ifndef COLORPATTERNLIB_H
 #define COLORPATTERNLIB_H
 
+// This header file is only in place to link to the other files in the common lib.
+
 #include "ColorPattern.h"
 #include "ColorFadePattern.h"
 #include "RainbowColorPattern.h"
