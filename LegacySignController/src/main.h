32d617f296aff26cec7daa47d60bdfd19ba784bf
Larry Moore <lfmoore3@hotmail.com>
2025-03-16 10:27:54 -0400

    Add dual core support for Legacy sign controller (#20)
    * Move LED updates to second core
    
    * Update pin comments

--- LegacySignController/src/main.h
+++ LegacySignController/src/main.h
@@ -10,22 +10,22 @@
 #include <PixelBufferLib.h>
 
 #define DATA_OUT 25            // GPIO pin # (NOT Digital pin #) controlling the NeoPixels
-#define VOLTAGEINPUTPIN 17     // The pin # (Digital #) for the analog input to detect battery voltage level.
+#define VOLTAGEINPUTPIN 29     // GPIO pin # for the analog input to detect battery voltage level.
 
-#define TM1637_CLOCK 11    // Digital pin # for the TM1637 clock line
-#define TM1637_DIO 10      // Digital pin # for the TM1637 data line
+#define TM1637_CLOCK 7    // GPIO pin # for the TM1637 clock line
+#define TM1637_DIO 5      // GPIO pin # for the TM1637 data line
 #define TM1637_BRIGHTNESS 5  // Brightness of the TM1637, between 0 and 7
 
 #define LOWPOWERTHRESHOLD 6.7     // The voltage below which the system will go into "low power" mode.
 #define NORMALPOWERTHRESHOLD 7.2  // The voltage above which the system will recover from "low power" mode.
 #define VOLTAGEMULTIPLIER 4.83    // The value to multiply the analog reading by to get the actual voltage.
 
-#define MANUAL_INPUT_PINS 15, 16, 14      // Digital pin #s for the manual input buttons.
+#define MANUAL_INPUT_PINS 27, 28, 26      // GPIO pin #s for the manual input buttons.
 
 #define TELEMETRYINTERVAL 2000     // Interval (msec) for updating the telemetry.
 
 #define DEFAULT_BRIGHTNESS 255  // Brightness should be between 0 and 255.
-#define LOW_BRIGHTNESS_PIN  12  // When this Digital pin # is pulled low, the default brightness will be much lower than normal.
+#define LOW_BRIGHTNESS_PIN  4  // When this GPIO pin # is pulled low, the default brightness will be much lower than normal.
 #define DEFAULT_BRIGHTNESS_LOW 20 // The default brightness to use when the "Low Brightness" pin has been pulled low.
 
 #define LEGACY_SIGN_TYPE 16     // The "sign type" corresponding to the Legacy sign, used to initialize the pixel buffer.
