8395c346d3a4ee55380f37b60ffd8feff9c41755
Larry Moore <lfmoore3@hotmail.com>
2025-11-29 10:33:00 -0500

    Set primary and alt SD Card pins

--- LegacySignController/src/main.h
+++ LegacySignController/src/main.h
@@ -16,13 +16,14 @@
 /*
 Valid GPIO pins for SPI0:
 CLK: 2, 6, 18, 22
-CIPO: 1, 5, 17, 21
+CIPO: 0, 4, 16, 20
 COPI: 3, 7, 19, 23
  */
-#define SDCARD_CHIPSELECT  20
-#define SDCARD_SPI_CLOCK  18
-#define SDCARD_SPI_COPI   19
+#define SDCARD_CHIPSELECT  18
+#define SDCARD_SPI_CLOCK  6
+#define SDCARD_SPI_COPI   7
 #define SDCARD_SPI_CIPO    4
+// The legacy sign uses the default COPI pin for the Status Display CLK signal.
 #define SDCARD_ALT_CHIPSELECT  20
 #define SDCARD_ALT_SPI_CLOCK  18
 #define SDCARD_ALT_SPI_COPI   19
