acfaeabd9c19e1e5fb59a4f72119dc38b938e135
Larry Moore <lfmoore3@hotmail.com>
2025-12-18 08:30:40 -0500

    Add JsonUtils to Utils.h

--- LegacySignController/src/Utils/Utils.h
+++ LegacySignController/src/Utils/Utils.h
@@ -1,8 +1,9 @@
 #ifndef COMMONUTILS_H
 #define COMMONUTILS_H
 
+#include "DebugUtils.h"
+#include "JsonUtils.h"
 #include "MathUtils.h"
 #include "NeoPixelUtils.h"
-#include "DebugUtils.h"
 
 #endif
